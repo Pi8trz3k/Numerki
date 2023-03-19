4abc2037a44e2072ce4d3e060c0a7d3336d85d56
Kacper Pietrzak <pietrzak2001kacper@o2.pl>
2023-03-19 20:39:43 +0100

    Newton

--- Zadanie 1/functions.py
+++ Zadanie 1/functions.py
@@ -47,30 +47,31 @@ def deriv_polynominal_exponential(number):
     return 3 * (2 ** number + 2 ** (3 * number + 2)) * math.log(2, math.e)
 
 
-def choice(func_number, stop_condition_number, stop_condition, a, b):
+def choice(func_number, stop_condition_number, stop_condition, a, b, newton_guess):
     match func_number:
-        # # variant B -> |f(xi)| < e
-        # case 1:
-        #     return 0
-        # # user defined iterations
-        # case 2:
-        #     return 0
+        # variant B -> |f(xi)| < e
         case 1:
             bisection = bisection_method(polynominal, stop_condition_number, stop_condition, a, b)
-            # newton = newton_method(polynominal, deriv_polynominal, stop_condition_number, stop_condition, )
-            return bisection
+            newton = newton_method(polynominal, deriv_polynominal, stop_condition_number, stop_condition, newton_guess)
+            return bisection, newton
         case 2:
             bisection = bisection_method(cos, stop_condition_number, stop_condition, a, b)
-            return bisection
+            newton = newton_method(cos, deriv_cos, stop_condition_number, stop_condition, newton_guess)
+            return bisection, newton
         case 3:
             bisection = bisection_method(exponential, stop_condition_number, stop_condition, a, b)
-            return bisection
+            newton = newton_method(exponential, deriv_exponential, stop_condition_number, stop_condition, newton_guess)
+            return bisection, newton
         case 4:
             bisection = bisection_method(polynominal_cos, stop_condition_number, stop_condition, a, b)
-            return bisection
+            newton = newton_method(polynominal_cos, deriv_polynominal_cos, stop_condition_number, stop_condition,
+                                   newton_guess)
+            return bisection, newton
         case 5:
             bisection = bisection_method(polynominal_exponential, stop_condition_number, stop_condition, a, b)
-            return bisection
+            newton = newton_method(polynominal_exponential, deriv_polynominal_exponential, stop_condition_number,
+                                   stop_condition, newton_guess)
+            return bisection, newton
         case _:
             return "Cos sie popsulo"
 
@@ -96,7 +97,7 @@ def bisection_method(function, stop_condition_number, stop_condition, a, b):
         print("ABS: ", abs(b-a), "a, b: ", a, b)
     # liczba iteracji
     elif stop_condition_number == 2:
-        for _ in range(1, stop_condition):
+        for i in range(0, stop_condition):
             x = (a + b) / 2.0
             if function(x) == 0:
                 return x
@@ -109,18 +110,23 @@ def bisection_method(function, stop_condition_number, stop_condition, a, b):
             elif function(x) * function(b) < 0:
                 a = x
         print("ABS: ", abs(b - a), "a, b: ", a, b)
-    return results
+    return x
 
 
 def newton_method(function, funcderiv, stop_condition_number, stop_condition_type, x):
     results = []
-
+    print("Metoda Newtona:")
     # dokładność
     if (stop_condition_number == 1):
-        return 0
+        while abs(function(x)) > stop_condition_type:
+            xi = x - (function(x)/funcderiv(x))
+            x = xi
+            print("newton x: ", xi)
     # liczba iteracji
     elif (stop_condition_number == 2):
-        for iter in range(1, stop_condition_type):
-            i = x - (function(x)/funcderiv(x))
+        for i in range(0, stop_condition_type):
+            xi = x - (function(x)/funcderiv(x))
+            x = xi
+            print("newton x: ", xi)
 
-    return results
+    return x
