f4b2a05a2bb884038a35bd2b976c635e83ce14e3
Kacper Pietrzak <pietrzak2001kacper@o2.pl>
2023-04-13 10:52:28 +0200

    Refactoring

--- Zadanie 2/functions.py
+++ Zadanie 2/functions.py
@@ -7,18 +7,10 @@ def is_diagonally_dominant(array):
     for i in range(tab_len):
         row_sum = np.sum(abs(array[i]))
         print(row_sum)
-        for j in range(tab_len):
-            if i == j:
-                diagonal_number = array[i][j]
-                row_sum_without_diagonal_number = row_sum - diagonal_number
-                if diagonal_number < row_sum_without_diagonal_number:
-                    return False
-                print("BBB", row_sum_without_diagonal_number, "| diagonall", diagonal_number)
-                # print(diagonal_number)
-                # sum of row - last number in row that is result of the equation -
-
-            # print(table[i][j])
-    # print(tab_len)
-
+        diagonal_number = abs(array[i][i])
+        row_sum_without_diagonal_number = row_sum - diagonal_number
+        if diagonal_number <= row_sum_without_diagonal_number:
+            return False
+    return True
 
 
