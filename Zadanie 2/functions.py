d90561e4fecc39df783d47c5f2b618fdbddd0144
Kacper Pietrzak <pietrzak2001kacper@o2.pl>
2023-04-13 10:53:25 +0200

    Adding function that makes array diagonally dominant

--- Zadanie 2/functions.py
+++ Zadanie 2/functions.py
@@ -25,4 +25,28 @@ def get_array_without_results_numbers(array):
     return coefficients
 
 
+def make_diagonally_dominant(array):
+    array_len = len(array)
+    # array that have ones inside, used later
+    # zeros_array = np.ones(array_len)
+    #
+    # for i in range(array_len):
+    #     row_sum = np.sum(abs(array[i]))
+    #     diagonal_number = abs(array[i][i])
+    #     if diagonal_number < row_sum - diagonal_number:
+    #         zeros_array[i] = 0
+    #
+    # print(zeros_array)
+    for i in range(array_len):
+        row_sum = np.sum(np.abs(array[i]) - np.abs(array[i][i]))
+        if np.abs(array[i, i]) <= row_sum:
+            max_index = np.argmax(np.abs(array[i, :]))
+            array[[i, max_index], :] = array[[max_index, i], :]
+    return array
 
+
+coefficients_with_results = np.loadtxt("./coefficients.txt", dtype="int", delimiter=",")
+coefficients_only = get_array_without_results_numbers(coefficients_with_results)
+# print(is_diagonally_dominant(coefficients_only))
+# print(coefficients_only)
+print(make_diagonally_dominant(coefficients_only))
