979311e39c32c2b46f0a5680c7f70b538cd2fd8a
Kacper Pietrzak <pietrzak2001kacper@o2.pl>
2023-04-14 10:57:05 +0200

    Complete refactoring

--- Zadanie 2/functions.py
+++ Zadanie 2/functions.py
@@ -50,92 +50,76 @@ def make_diagonally_dominant(coefficients_array, results):
     return coefficients_array, results
 
 
-# first
-# def gauss_seidel(condition_choose, number_choose):
-#     array = np.loadtxt("./coefficients.txt", dtype="float", delimiter=",", encoding="utf-8")
-#     coefficients_array = get_coefficients(array)
-#     result_array = get_constants(array)
-#
-#     if not (is_diagonally_dominant(coefficients_array)):
-#         coefficients_array, result_array = make_diagonally_dominant(coefficients_array, result_array)
-#
-#     coefficents_array_len = len(coefficients_array)
-#     x = np.zeros(coefficents_array_len)
-#     x_new = np.zeros(coefficents_array_len)
-#
-#     match condition_choose:
-#         # iterations
-#         case 1:
-#             for i in range(number_choose):
-#                 for j in range(coefficents_array_len):
-#                     # tablica A[wiersz j, kolumny do j]; tablica x_new[do kolumny j]
-#                     # U*x^i
-#                     ux = np.dot(coefficients_array[j, :j], x_new[:j])
-#                     # tablica A[wiersz j, kolumny od(j + 1)]; tablica x[kolumny od (j+1)]
-#                     # -L*x^(i+1)
-#                     lxi_1 = np.dot(coefficients_array[j, j + 1:], x[j + 1:])
-#                     x_new[j] = (result_array[j] - ux - lxi_1) / coefficients_array[j, j]
-#                     # print("x_new, iteracja j:", j, x_new)
-#                 x = x_new
-#             return x
-#         case 2:
-#             while True:
-#                 for j in range(coefficents_array_len):
-#                     # tablica A[wiersz j, kolumny do j]; tablica x_new[do kolumny j]
-#                     # U*x^i
-#                     ux = np.dot(coefficients_array[j, :j], x_new[:j])
-#                     # tablica A[wiersz j, kolumny od(j + 1)]; tablica x[kolumny od (j+1)]
-#                     # -L*x^(i+1)
-#                     lxi_1 = np.dot(coefficients_array[j, j + 1:], x[j + 1:])
-#                     x_new[j] = (result_array[j] - ux - lxi_1) / coefficients_array[j, j]
-#                     # print("x_new, iteracja j:", j, x_new)
-#                     if np.allclose(x, x_new, atol=number_choose):
-#                         return x_new
-#                 x = x_new
+def get_diagonal_reversed(array_input):
+    array_len = len(array_input)
+    return_array = np.zeros((array_len, array_len))
+
+    for i in range(array_len):
+        return_array[i][i] = 1 / array_input[i][i]
+
+    return return_array
 
 
-# second
-def gauss_seidel(A, b, x0, epsilon, max_iterations):
-    n = len(A)
-    x = x0.copy()
+def get_under_diagonal(array_input):
+    array_len = len(array_input)
+    return_array = np.zeros((array_len, array_len))
+
+    for i in range(array_len):
+        for j in range(array_len):
+            if j < i:
+                return_array[i][j] = array_input[i][j]
+    return return_array
+
+
+def get_over_diagonal(array_input):
+    array_len = len(array_input)
+    return_array = np.zeros((array_len, array_len))
+
+    for i in range(array_len):
+        for j in range(array_len):
+            if j > i:
+                return_array[i][j] = array_input[i][j]
+    return return_array
+
+
+def gauss_siedl(max_iterations):
+    array = np.loadtxt("./coefficients.txt", dtype="float", delimiter=",", encoding="utf-8")
+    coefficients = get_coefficients(array)
+    constants = get_constants(array)
+
+    if not (is_diagonally_dominant(coefficients)):
+        coefficients, constants = make_diagonally_dominant(coefficients, constants)
+        if not (is_diagonally_dominant(coefficients)):
+            print(coefficients)
+            print("Macierz nie jest dominująca przekątniowo, program zostaje zakonczony")
+            quit()
+
+    array_len = len(coefficients)
+
+    x = np.zeros(array_len)
+
+    diagonal_reversed = get_diagonal_reversed(coefficients)
+    under_diagonal = get_under_diagonal(coefficients)
+    over_diagonal = get_over_diagonal(coefficients)
+
+    db = np.dot(diagonal_reversed, constants)
+    dl = np.dot(diagonal_reversed, under_diagonal)
+    du = np.dot(diagonal_reversed, over_diagonal)
+
+    x_new = np.zeros(array_len)
 
     for i in range(max_iterations):
-        x_new = np.zeros(n)
-        for j in range(n):
-            s1 = np.dot(A[j, :j], x_new[:j])
-            s2 = np.dot(A[j, j + 1:], x[j + 1:])
-            x_new[j] = (b[j] - s1 - s2) / A[j, j]
-        if np.allclose(x, x_new, rtol=epsilon):
-            return x_new
-        x = x_new
+
+        for k in range(len(db)):
+            dl_numbers = 0
+            du_numbers = 0
+            for col in range(array_len):
+                dl_numbers = dl_numbers + (-1 * dl[k][col] * x[col])
+                du_numbers = du_numbers + (-1 * du[k][col] * x_new[col])
+            x_new[k] = (db[k] + dl_numbers + du_numbers)
+            x = x_new
+
     return x
 
 
-def start(choice, number):
-    print()
-    # first method
-    # print("One", gauss_seidel(1, 1))
-    # print("Two", gauss_seidel(2, 0.000001))
-
-    # second method
-    # array = np.loadtxt("./coefficients.txt", dtype="float", delimiter=",", encoding="utf-8")
-    # coefficients = get_coefficients(array)
-    # constants = get_constants(array)
-    # x = np.zeros(len(coefficients))
-    # if not(is_diagonally_dominant(array)):
-        # coefficients, constants = make_diagonally_dominant(coefficients, constants)
-    # gauss_seidel(coefficients, constants, number, 10)
-
-
-# print("One", gauss_seidel(coefficients, constants, x, 0.001, 100))
-# print("Two", gauss_seidel(coefficients, constants, x, 0.000001, 10000))
-
-
-# print("One", gauss_seidel(coefficients, constants, x, 0.001, 10))
-
-#
-# print("One", gauss_seidel(0.001, 10))
-# print("Two", gauss_seidel(0.000001, 10))
-
-
-# print("Two", gauss_seidel(coefficients, constants, 2, 0.001))
+print(gauss_siedl(9))
