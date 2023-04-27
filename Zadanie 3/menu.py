a2202e30bb37f58b0c2c54880a25b4c2f9fca8b5
Pi8trz3k <pietrzak2001kacper@o2.pl>
2023-04-27 19:57:54 +0200

    Added show menu and getting nodes

--- Zadanie 3/menu.py
+++ Zadanie 3/menu.py
@@ -1,26 +1,46 @@
 import math_functions as f
+import newtonInterpolation
 
 
-def functions(choice, number):
+def show():
+    print("1. Wielomian        -3 + 3x + 4x^3")
+    print("2. Liniowa          3x - 5")
+    print("3. Trygonometryczna cos(x - 0.1) + 2")
+    print("4. Moduł            |x - 1|")
+    print("5. Złożenie f4(f3)  |cos(x - 0.1) + 2 - 1|")
+    print("6. Złożenie f2(f4)  3(|x - 1|)- 5")
+    print("7. Złożenie f1(f2)  -3 + 3 * ( 3x - 5 ) + 4( 3x - 5)^3")
+
+
+def functions(factor):
+    choice = 0
+    show()
     while choice > 7 or choice < 1:
         print("Podaj swoj wybor: ")
         choice = int(input())
 
     match choice:
         case 1:
-            return f.polynominal(number)
+            return f.polynominal(factor)
         case 2:
-            return f.linear(number)
+            return f.linear(factor)
         case 3:
-            return f.cos(number)
+            return f.cos(factor)
         case 4:
-            return f.modulus(number)
+            return f.modulus(factor)
         case 5:
-            return f.modulus_cos(number)
+            return f.modulus_cos(factor)
         case 6:
-            return f.linear_modulus(number)
+            return f.linear_modulus(factor)
         case 7:
-            return f.polynominal_linear(number)
+            return f.polynominal_linear(factor)
         case _:
             print("Cos jest nie tak")
 
+
+def get_nodes():
+    return int(input("Podaj liczbę punktów: "))
+
+
+nodes = get_nodes()
+functions(nodes)
